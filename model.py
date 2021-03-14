049ac78613aed4d1b19924a230dcf0fe112c06f5
Krzysztof Wicki <krzysztof_wicki@sevenet.pl>
2021-03-14 21:58:55 +0100

    Naprawa bdu wywietlania wykresu

--- model.py
+++ model.py
@@ -2,7 +2,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import os
+import os 
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+
 
 class Linear_QNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
